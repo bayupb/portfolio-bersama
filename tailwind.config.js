19c229fb0d8614151a454a917be06596cd12ca3a
bayupb <kampuspb.bayu@gmail.com>
2021-12-09 13:51:51 +0700

    Portfolio

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    fontFamily: {
+      'rubik': ['Rubik', 'sans-serif'],
+      'comfortaa': ['Comfortaa', 'sans-cursive'],
+      'kanit': ['Kanit', 'sans-serif'],
+    },
+    extend: {},
+  },
+  variants: {
+    extend: {
+      backgroundColor: ['group-focus'],
+    },
+  },
+  plugins: [],
+}
